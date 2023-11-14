d0af111ba467e4b1d32857a7f3d2e98e135e2b52
firsaaa <118461789+firsaaa@users.noreply.github.com>
2023-11-14 15:26:01 +0700

    Update drivermesinkata.c

--- src/ADT/Mesin/drivermesinkata.c
+++ src/ADT/Mesin/drivermesinkata.c
@@ -3,17 +3,7 @@
 
 int main(){
     printf("Masukkan kata : ");
-
-    STARTINPUT();
-    Word input = GetWord();
-    ADVWORD();
-
-    while (!EndWord)
-    {
-        concatWord(&input, currentWord);
-        ADVWORD();
-    }
-
-    PrintWord(input);
+    GetCommand();
+    PrintWord(currentWord);
     printf("\n");
 }
