3fffebd7b3234c9ac8730a1bf29e96ee7d865904
jijiau <79503241+jijiau@users.noreply.github.com>
2023-11-17 12:08:06 +0700

    Update IsMember

--- src/ADT/Queue/queue.h
+++ src/ADT/Queue/queue.h
@@ -5,7 +5,7 @@
 #define QUEUE_H
 
 #include "../../boolean.h"
-#include "../Mesin/mesinkata.h"
+#include "../Mesin/MesinKata.h"
 
 #define IDX_UNDEF -1
 #define CAPACITY 100
@@ -38,7 +38,6 @@ void CreateQueue(Queue *q);
 /* ********* Prototype ********* */
 boolean queue_isEmpty(Queue q);
 /* Mengirim true jika q kosong: lihat definisi di atas */
-
 boolean queue_isFull(Queue q);
 /* Mengirim true jika tabel penampung elemen q sudah penuh */
 /* yaitu IDX_TAIL akan selalu di belakang IDX_HEAD dalam buffer melingkar*/
@@ -53,20 +52,19 @@ void enqueue(Queue *q, ElType val);
 /* F.S. val menjadi TAIL yang baru, IDX_TAIL "mundur" dalam buffer melingkar. */
 
 void queue_insert(Queue *q, ElType X);
-/* Proses: Menambahkan X pada q sebagai indeks pertama */
+/* Proses: Menambahkan val pada q sebagai indeks pertama */
 /* I.S. q mungkin kosong, tabel penampung elemen q TIDAK penuh */
 /* F.S. val menjadi HEAD yang baru, IDX_TAIL "mundur" dalam buffer melingkar. */
-
-void queue_deleteAt(Queue *q, int idx);
-/* Proses: Menghapus X pada q */
-/* I.S. q tidak mungkin kosong */
-/* F.S. idx = indeks pada queue yang ingin dihapus pd I.S., IDX_TAIL "mundur";
-        q mungkin kosong */
 
 void dequeue(Queue *q, ElType *val);
 /* Proses: Menghapus val pada q dengan aturan FIFO */
 /* I.S. q tidak mungkin kosong */
 /* F.S. val = nilai elemen HEAD pd I.S., IDX_HEAD "mundur";
         q mungkin kosong */
+
+/* *** Display Queue *** */
+
+boolean queue_IsMember(Queue q, int a);
+/* Mengirimkan true jika a adalah indeks di dalam list*/
 
 #endif
