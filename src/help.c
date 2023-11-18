0ecb7f051a08d4fddd4c79691f91961d39bdde45
firsaaa <118461789+firsaaa@users.noreply.github.com>
2023-11-18 17:28:49 +0700

    Update help.c

--- src/help.c
+++ src/help.c
@@ -10,11 +10,11 @@ void help(int inSession){
     else {
         printf("=====[ Menu Help WayangWave ]=====\n");
         printf("1. LIST -> Untuk menampilkan daftar lagu, playlist…\n");
-        printf("2. PLAY -> Untuk memulai suatu lagu");
-        printf("3. QUEUE -> \n");
-        printf("4. SONG -> \n");
-        printf("5. PLAYLIST -> \n");
-        printf("6. STATUS -> \n");
+        printf("2. PLAY -> Untuk memulai suatu lagu\n");
+        printf("3. QUEUE -> Untuk menampilan antrian lagu\n");
+        printf("4. SONG -> Untuk menampilkan lagu\n");
+        printf("5. PLAYLIST -> Untuk menampilkan playlist\n");
+        printf("6. STATUS -> Untuk menampilkan status\n");
         printf("7. SAVE -> Untuk menyimpan state ke dalam file\n");
         printf("8. QUIT -> Untuk keluar dari sesi\n");
     } 
