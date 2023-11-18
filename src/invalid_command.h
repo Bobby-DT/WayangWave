eeeed378d2cce32d527347794b0a48aad27db72d
firsaaa <118461789+firsaaa@users.noreply.github.com>
2023-11-18 17:26:01 +0700

    Add invalid_command

---
+++ src/invalid_command.h
@@ -0,0 +1,13 @@
+#ifndef INVALID_COMMAND_H
+#define INVALID_COMMAND_H
+
+#include <stdio.h>
+#include <string.h>
+
+void invalidCommand(const char *command);
+/*
+Fungsi ini membantu pengguna dengan memberikan informasi tentang 
+operasi-operasi yang dapat mereka lakukan dalam program WayangWave, 
+tergantung pada apakah sedang dalam sesi atau tidak.
+*/
+#endif /* INVALID_COMMAND_H */
