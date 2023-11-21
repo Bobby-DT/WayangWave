cd97e0a532c15d8a08b989c5afd25c5cc22f7434
Jihan <jihanaurelia2004@gmail.com>
2023-11-21 21:55:59 +0700

    benerin kurung

--- src/play.c
+++ src/play.c
@@ -90,6 +90,7 @@ void play(TabKata *Penyanyi, Queue *Antrian, Stack *Riwayat, Map *Album, Map *La
         printf("Berhasil menambahkan playlist \"");
         PrintWord(playlistTitle);
         printf("\" ke queue.\n\n");
+        }
     } else {
         printf("Command tidak diketahui!\n\n");
     }
